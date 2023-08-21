1f12cccdb57b251911deaf7a1463942b1de10e6f
kabeer11000 <kabeer@kabeersnetwork.tk>
2023-08-21 20:43:55 +0500

    updated and fixed

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,91 +2,92 @@ console.log("REGISTERED");
 
 let cacheData = "appv1"
 
-self.addEventListener("install",event => {
+self.addEventListener("install", event => {
 
-event.waitUntil(
-    caches.open('static').then(
-        vm => {vm.addAll([
-            '/images/image1',
-            '/images/image2',
-            '/Offline.js',
-            '/App.css',
-            '/'
-        ])}
-        
+    event.waitUntil(
+        caches.open('static').then(
+            vm => {
+                vm.addAll([
+                    '/images/image1',
+                    '/images/image2',
+                    '/Offline.js',
+                    '/App.css',
+                    '/'
+                ])
+            }
+        )
     )
-)
 })
 
-function onFetch(event){
-    let request = event.request
-    let header = request.headers.get('Accept')
-    let cacheKey = 'static'
+async function onFetch(event) {
+    // Updated this function
+    let request = event.request;
 
-    if(header.indexOf('text/html') !== -1)
-     cacheKey = 'content'
-    else if(header.indexOf('image') !== -1)
-    cacheKey = 'image'
-    
-    if(cacheKey === 'content'){
-        event.respondWith(
-        fetch(request)
-        .then(res => addToCache(cacheKey,res,request))
-        .catch(()=> getFromCache(event))
-        .catch(()=> showOffline())
-    )}
-    else{
-        event.respondWith(
-        getFromCache(event)
-        .catch(()=> fetch(request))
-        .then(res=> addToCache(cacheKey,res,request))
-        .catch(()=> showOffline())
-    )}
+    // Check if the browser is online
+    if (navigator.onLine) {
+        // If online, fetch the resource and store it in the cache
+        return fetch(request)
+            .then(res =>
+                caches.open('static-1')
+                    .then(cache => cache.put(request, res.clone()))
+                    .then(() => res) // Return the fetched response
+            );
+    }
 
+    // If offline, attempt to retrieve the resource from the cache
+    const cachedRes = (await caches.match(request)) ?? null;
+
+    // Create a new response with additional information indicating offline status
+    return new Response(
+        JSON.stringify({
+            ...(await cachedRes.json()), // Merge cached response data with offline flag
+            offline: true
+        }),
+        {
+            status: cachedRes.status,
+            statusText: cachedRes.statusText,
+            headers: cachedRes.headers
+        }
+    );
 }
 
-function getFromCache(event){
+function getFromCache(event) {
 
     return caches.match(event.request).then(resp => {
-        if(!resp){
-        throw Error("Something Went Wrong")}
+            if (!resp) {
+                throw Error("Something Went Wrong")
+            }
 
-        return resp}
-    )}
-    
+            return resp
+        }
+    )
+}
 
 
-function addToCache(key,response,request){
+function addToCache(key, response) {
     let copy = response.clone()
-    if(response.ok){
-        caches.open(key).then(cache => cache.put(key,copy))
+    if (response.ok) {
+        caches.open(key).then(cache => cache.put(key, copy))
     }
-  return response
+    return response
 }
 
-function shouldHandleFetch (event) {
-    var request            = event.request;
-    var url                = new URL(request.url);
-    var criteria           = {
-      isGETRequest      : request.method === 'GET',
-      isFromMyOrigin    : url.origin === self.location.origin
+function shouldHandleFetch(event) {
+    const request = event.request;
+    const url = new URL(request.url);
+    const criteria = {
+        isGETRequest: request.method === 'GET',
+        isFromMyOrigin: url.origin === self.location.origin
     };
-  
-    var failingCriteria    = Object.keys(criteria)
-      .filter(criteriaKey => !criteria[criteriaKey]);
-     
-      console.log("RETURNED STATUS",!failingCriteria.length);
+
+    const failingCriteria = Object.keys(criteria)
+        .filter(criteriaKey => !criteria[criteriaKey]);
+
+    console.log("RETURNED STATUS", !failingCriteria.length);
     return !failingCriteria.length;
-  }
-
-
-self.addEventListener("fetch",ev => {
-
-
-console.log("SOMETHING GOT FETCHED",ev);
-
-    if(shouldHandleFetch(ev)){ 
-    onFetch(ev)
 }
-    console.log("SOMEWHERE FETCH OCCURED!")
+
+
+self.addEventListener("fetch", event => {
+    event.respondWith(onFetch(event));
 })
